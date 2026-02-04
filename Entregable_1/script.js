a23cfb3cc74e430a31f308b65f4edda51c376874
DhidelOsorio <djosorio@ufm.edu>
2026-02-03 22:46:30 -0600

    Exercise 1
    The CV format was changed and a dynamic greeting was implemented.

---
+++ Entregable_1/script.js
@@ -0,0 +1,15 @@
+// Saludo 
+
+let hora = new Date().getHours();
+
+let temprano = ("Buenos días");
+let tarde = ("Buenas tardes");
+let noche = ("Buenas noches");
+function saludo() {
+    let texto; 
+    if (hora <= 12){texto = temprano;}
+    else if (hora >= 13 && hora <= 18) {texto = tarde;}
+    else if (hora >= 19 && hora <= 23){texto = noche;}
+    document.getElementById("saludo").textContent = texto;
+    }
+saludo();
