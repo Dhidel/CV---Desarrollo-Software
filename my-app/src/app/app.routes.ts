34429d8429bdfe5fd3c43d4ed7f71426781314a3
DhidelOsorio <djosorio@ufm.edu>
2026-02-21 18:46:03 -0600

    Exercise 1
    i added routs in all cv

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Personal } from './components/personal/personal';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Studies } from './components/studies/studies';
+import { Contacts } from './components/contacts/contacts';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'personal', pathMatch: 'full' },
+    {path: 'personal', component: Personal},
+    {path: 'skills', component: Skills},
+    {path: 'projects', component: Projects},
+    {path: 'studies', component: Studies},
+    {path: 'contacts', component: Contacts}
+]
