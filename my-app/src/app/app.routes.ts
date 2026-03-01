af85e96389c6b1bc992f75bd5ba492b738a2c3d0
DhidelOsorio <djosorio@ufm.edu>
2026-03-01 15:54:24 -0600

    Exercise 2
    I finished the GitHub API service and my repositories are now displayed.

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -8,19 +8,21 @@ import { Contacts } from './components/contacts/contacts';
 import { NgModule } from '@angular/core';
 import { NxtBoardDetail } from './components/projects/nxt-board-detail/nxt-board-detail';
 import { CvDetail } from './components/projects/cv-detail/cv-detail';
+import { GithubComponent } from './components/github/github';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'personal', pathMatch: 'full' },
-    {path: 'personal', component: Personal},
-    {path: 'skills', component: Skills},
+    { path: 'personal', component: Personal },
+    { path: 'skills', component: Skills },
     {
-    path: 'projects',
-    component: Projects,
-    children: [
-      { path: 'nxt-board', component: NxtBoardDetail},
-      { path: 'cv', component: CvDetail }
-    ]
-  },
-    {path: 'studies', component: Studies},
-    {path: 'contacts', component: Contacts}
-]
+        path: 'projects',
+        component: Projects,
+        children: [
+            { path: 'nxt-board', component: NxtBoardDetail },
+            { path: 'cv', component: CvDetail },
+            { path: 'github', component: GithubComponent } 
+        ]
+    },
+    { path: 'studies', component: Studies },
+    { path: 'contacts', component: Contacts }
+];
--- my-app/src/app/services/github.ts
+++ my-app/src/app/services/github.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Github {
-  
+@Injectable({ providedIn: 'root' })
+export class GithubService {
+  private apiUrl = 'https://api.github.com/users/Dhidel/repos'; 
+
+  constructor(private http: HttpClient) {}
+
+  getRepos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
